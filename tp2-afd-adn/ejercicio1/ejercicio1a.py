384eef6cb18ff5c736f8aed3f030d4ac9ef04f3b
Gaston Fenske <gastonfenske@MacBook-Pro-de-Gaston.local>
2022-04-05 19:24:57 -0300

    antes de empezar a cambiar el ejercicio1b

---
+++ tp2-afd-adn/ejercicio1/ejercicio1a.py
@@ -0,0 +1,35 @@
+from estadosej1a import estados
+ 
+class Automata:
+
+    """(a|b)*"""
+    def __init__(self, string: str) -> None:
+        self.string = string
+
+    def proof(self) -> bool:
+
+        correct = False
+        index = 0
+        accept_states = [3,4]
+
+        for c in self.string:
+            try:
+                index = estados[index][c]
+            except KeyError:
+                """Aca solo entra si esa opcion no esta disponible en el estado actual"""
+                correct = False
+                return correct
+            if index in accept_states:
+                """Aca entra si se llego al estado de aceptacion"""
+                correct = True
+            else:
+                correct = False
+        return correct
+
+if __name__ == '__main__':
+    automata = Automata(string='bbbba')
+    proof = automata.proof()
+    if proof:
+        print(f"La cadena: [{automata.string}] esta bien ")
+    else:
+        print(f"La cadena: [{automata.string}] esta mal ")
