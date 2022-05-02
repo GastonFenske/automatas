1f005755ad0f4d3a93731d1837363a490f1c175a
Gaston Fenske <gastonfenske@MacBook-Pro-de-Gaston.local>
2022-05-02 17:06:21 -0300

    agregue que la contraseña tenga al menos una minuscula

--- tp4/contra.py
+++ tp4/contra.py
@@ -1,7 +1,7 @@
 import re
 
 NOMBRE_ARCHIVO = 'contras.txt'
-PATRON = '(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}'
+PATRON = '(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}'
 
 class Analizador:
 
